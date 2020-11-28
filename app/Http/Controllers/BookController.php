1c644a43ac3aa0415bf12a2c4613c9eda2fce5c3
bianca-soares <biancasoaresdasilva2016@gmail.com>
2020-11-28 13:32:40 -0300

    teste

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -13,7 +13,7 @@ class BookController extends Controller
      */
     public function index()
     {
-        return view('index');//
+        return vew('index');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'BookController@index');
+Route::get('/', function(){
+    return view('index');
+});
